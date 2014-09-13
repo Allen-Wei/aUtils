8a02d7f429e8cd08c54efe76ad5c10a6fef1c38a
Alan <Allen-Wei@live.com>
2014-09-13 16:29:45 +0800

    update via webstorm

---
+++ javascripts/scripts/aUtils.Telerik.js
@@ -0,0 +1,90 @@
+﻿
+
+
+
+(function($) {
+    if (!window.aUtils) window.aUtils = function() {};
+    aUtils.RadGridClient = function(arg1) {
+        this.clientId = '';
+        this.table = undefined;
+        this.masterTable = undefined;
+        if (arg1 && arg1.indexOf) {
+            this.clientId = arg1;
+        } else {
+            this.table = arg1;
+        }
+
+        this.InitTable = function(identity) {
+            //this.clientId = $('[id$=' + identity + ']').attr('id');
+            if (identity) this.clientId = identity;
+            this.table = $find(this.clientId);
+            return this;
+        };
+        //will expire
+        this.MastTable = function() {
+            if (!this.table) this.InitTable();
+            if (this.table) return this.table.get_masterTableView();
+            else return undefined;
+        };
+        this.InitMasterTable = function() {
+            if (!this.table) this.InitTable();
+            if (this.table) this.masterTable = this.table.get_masterTableView();
+            else this.masterTable = undefined;
+        };
+        this.Rebind = function() {
+            if (!this.masterTable) this.InitMasterTable();
+            try {
+                if (this.masterTable) {
+                    this.masterTable.rebind();
+                } else {
+                    console.log('radgrid bind fail');
+                }
+            } catch (ex) {
+                console.log(ex.message);
+            }
+            return this;
+        }
+    };
+
+})(jQuery);
+
+
+var RadGridClient = function (arg1) {
+    this.clientId = '';
+    this.table = undefined;
+    this.masterTable = undefined;
+    if (arg1 && arg1.indexOf) {
+        this.clientId = arg1;
+    } else {
+        this.table = arg1;
+    }
+
+    this.InitTable = function (identity) {
+        //this.clientId = $('[id$=' + identity + ']').attr('id');
+        if (identity) this.clientId = identity;
+        this.table = $find(this.clientId);
+        return this;
+    };
+    //will expire
+    this.MastTable = function () {
+        if (!this.table) this.InitTable();
+        if (this.table) return this.table.get_masterTableView();
+        else return undefined;
+    };
+    this.InitMasterTable = function () {
+        if (!this.table) this.InitTable();
+        if (this.table) this.masterTable = this.table.get_masterTableView();
+        else this.masterTable = undefined;
+    };
+    this.Rebind = function () {
+        if (!this.masterTable) this.InitMasterTable();
+        try {
+            if (this.masterTable) {
+                this.masterTable.rebind();
+            } else {
+                console.log('radgrid bind fail');
+            }
+        } catch (ex) { console.log(ex.message); }
+        return this;
+    }
+};
---
+++ javascripts/scripts/aUtils.XDM.js
@@ -0,0 +1,20 @@
+﻿
+(function($) {
+    if (!window.aUtils) window.aUtils = function() {};
+    aUtils.XDM = function(targetDoc) {
+        this.targetDoc = targetDoc;
+
+        var domain = 'http://' + location.host;
+        this.post = function(msg, tDoc) {
+            if (tDoc) {
+                tDoc.postMessage(msg, domain);
+            } else {
+                this.targetDoc.postMessage(msg, domain);
+            }
+        };
+        this.resetDoc = function(doc) {
+            this.targetDoc = doc;
+        }
+    };
+
+})(jQuery);
