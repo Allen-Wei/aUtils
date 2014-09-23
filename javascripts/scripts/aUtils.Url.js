6dcd978aa01f06d57bc5126e07c97763cb9c503b
Alan <Allen-Wei@live.com>
2014-09-23 19:08:03 +0800

    update via webstorm

--- javascripts/scripts/aUtils.Url.js
+++ javascripts/scripts/aUtils.Url.js
@@ -12,8 +12,9 @@
         if (url) {
             search = url.split('?')[1];
         }
-        if (!url) {
-            throw  new Error('cannot find search parameter.');
+        if (!search) {
+            console.log('cannot find search parameter.');
+            return {};
         }
 
         var paras = search.split('&');
@@ -27,8 +28,33 @@
         }
         return retParas;
     };
-    aUtils.Url.query = function(key, url){
+    aUtils.Url.query = function (key, url) {
         var params = aUtils.Url.getAll(url);
         return params[key];
     }
+    aUtils.Url.path = function (index) {
+        var urls = location.href
+            .replace('http://' + location.host, '')
+            .replace(location.search, '')
+            .replace(location.hash, '')
+            .split('/');
+
+        var array = [];
+        for (var i = 0; i < urls.length; i++) {
+            var url = urls[i];
+            if (url) {
+                array.push(url);
+            }
+        }
+        if (index === 0 || index) {
+            if (index >= 0) {
+                return array[index];
+            }
+            if (index < 0) {
+                return array[array.length + index];
+            }
+            throw new Error('index is error');
+        }
+        return array;
+    };
 })();
