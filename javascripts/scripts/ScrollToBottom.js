55c433fe22b52f54508aff8ab86457184864dd76
Alan <alan.wei@live.com>
2018-12-28 18:00:23 +0800

    滚动加载增加window兼容

--- javascripts/scripts/ScrollToBottom.js
+++ javascripts/scripts/ScrollToBottom.js
@@ -6,6 +6,9 @@ HTMLElement.prototype.onScrollToBottom = function (callback, threshold) {
         }
     }, false);
 };
+/**
+* 参考 https://stackoverflow.com/questions/14035180/jquery-load-more-data-on-scroll
+*/
 window.onScrollToBottom = function (callback, threshold) {
 	var sub = document.documentElement.offsetHeight - window.innerHeight - window.pageYOffset;
 	if (sub <= (threshold || 0)) {
